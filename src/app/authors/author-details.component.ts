317ccfa224b91299aaca5ed422fd83d3ce47c0fe
21120105 <nhantrnh03@gmail.com>
2024-06-10 11:25:45 +0700

    fix Output Binding

--- src/app/authors/author-details.component.ts
+++ src/app/authors/author-details.component.ts
@@ -1,18 +1,19 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { Author, authors } from "../authors.model";
+import { Author } from "../authors.model";
+
 @Component({
-    selector: 'author-details',
-    template: `
+  selector: 'author-details',
+  template: `
     <div>
-        {{author.firstName}}
-       {{author.lastName}}
-        <button style="margin-left: 10px;" (click)="select.emit(author)">Select</button>
+      {{author.firstName}} {{author.lastName}}
+      <button style="margin-left: 10px;" (click)="select.emit(author)">Select</button>
+      <button style="margin-left: 10px;" (click)="delete.emit(author)">Delete</button>
     </div>
-    `,
-    standalone: true,
+  `,
+  standalone: true,
 })
 export class AuthorDetailsComponent {
-    @Input() author!: Author;
-
-    @Output() select = new EventEmitter<Author>();
+  @Input() author!: Author;
+  @Output() select = new EventEmitter<Author>();
+  @Output() delete = new EventEmitter<Author>();
 }
--- src/app/authors/author-list.component.ts
+++ src/app/authors/author-list.component.ts
@@ -1,23 +1,37 @@
 import { Component } from "@angular/core";
 import { AuthorDetailsComponent } from "./author-details.component";
 import { authors, Author } from "../authors.model";
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
+
 @Component({
-    selector: 'author-list',
-    template: `<author-details *ngFor="let item of authors" [author]="item!" (select)="onSelected($event)"></author-details>
+  selector: 'author-list',
+  template: `
+    <author-details 
+      *ngFor="let item of authors" 
+      [author]="item" 
+      (select)="onSelected($event)" 
+      (delete)="onDelete($event)">
+    </author-details>
     <br>
     <div>
-    current selected author: {{currentAuthor?.firstName}} {{currentAuthor?.lastName}}
+      Current selected author: {{currentAuthor?.firstName}} {{currentAuthor?.lastName}}
     </div>
-    `,
-    standalone: true,
-    imports: [AuthorDetailsComponent],
+  `,
+  standalone: true,
+  imports: [AuthorDetailsComponent, CommonModule],
 })
 export class AuthorListComponent {
-    authors = authors;
-    currentAuthor = authors[0];
+  authors = authors;
+  currentAuthor: Author | null = authors[0];
 
-    onSelected(selectedAuthor: Author) {
-        this.currentAuthor = selectedAuthor
+  onSelected(selectedAuthor: Author) {
+    this.currentAuthor = selectedAuthor;
+  }
+
+  onDelete(author: Author) {
+    this.authors = this.authors.filter(a => a.id !== author.id);
+    if (this.currentAuthor && this.currentAuthor.id === author.id) {
+      this.currentAuthor = this.authors.length ? this.authors[0] : null;
     }
+  }
 }
